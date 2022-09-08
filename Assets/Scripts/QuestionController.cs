98824535aad074c861567f125b8328dfb4433987
Meyra <75731320+MeyraG@users.noreply.github.com>
2022-09-08 23:54:08 +0300

    improvements

--- Assets/Scripts/QuestionController.cs
+++ Assets/Scripts/QuestionController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Question", menuName = "QuestionController")]
 public class QuestionController : ScriptableObject
 {
-    public string questionText;
+    public List<string> questions = new List<string>();
+    //public string questionText;
     public string[] answers;
     public int correctAnswerIndex;
 }
